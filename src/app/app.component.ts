b251d38d469fd4e6de53799f20ed3df83038c5ae
Athira Abdul Razak <athiraabdulrazak@gmail.com>
2021-09-21 22:22:02 -0700

    Form Created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,52 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-pro';
+ 
   faCoffee = faCoffee;
+  submitted=false;
+  contactForm = this.fb.group({
+    contacts: this.fb.array([this.createContact()])
+  });
+  
+
+  constructor(private fb: FormBuilder) {}
+
+  get contacts() {
+    return this.contactForm.get('contacts') as FormArray;
+  }
+
+  createContact(): FormGroup {
+    return this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: [''],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['']
+    });
+  }
+  addContacts() {
+    this.contacts.push(this.createContact());
+  }
+  
+
+  deleteContact(i: number) {
+    this.contacts.removeAt(i)
+  }
+
+  onSubmit() {
+    this.submitted=true;
+    console.log(this.contactForm.value);
+  }
+  
 }
+
+
+
+ 
+  
+
+  
+
+  
+  
+
+
